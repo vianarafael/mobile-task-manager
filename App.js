0f47011ff4e74edf96a68dda45ffd339187ce590
Rafael Viana <vianarafaelds@gmail.com>
2021-10-20 17:27:43 +0900

    (WIP) firebase implementation

--- App.js
+++ App.js
@@ -3,6 +3,22 @@ import { StyleSheet, Text, View, ScrollView } from "react-native";
 import Activity from "./components/activity";
 import Profile from "./components/profile";
 
+import * as firebase from "firebase";
+
+// Optionally import the services that you want to use
+import "firebase/auth";
+//import "firebase/database";
+//import "firebase/firestore";
+//import "firebase/functions";
+//import "firebase/storage";
+
+// Initialize Firebase
+const firebaseConfig = {
+  // firebaseから発行される
+};
+
+firebase.initializeApp(firebaseConfig);
+
 export default function App() {
   return (
     <ScrollView>
@@ -10,10 +26,8 @@ export default function App() {
         <Profile />
         <Activity activity="Work" color="hsl(15, 100%, 70%)" />
         <Activity activity="Study" color="hsl(195, 74%, 62%)" />
-        <Activity activity="Exercise" color="hsl(348, 100%, 68%)" />
         <Activity activity="Personal Projects" color="hsl(145, 58%, 55%)" />
-        <Activity activity="Family" color="hsl(264, 64%, 52%)" />
-        <Activity activity="Play" color="hsl(43, 84%, 65%)" />
+        <Activity activity="Exercise" color="hsl(348, 100%, 68%)" />
       </View>
     </ScrollView>
   );
