6328e09c2f384721f7d2753354a2934a17e885a7
Rafael Viana <vianarafaelds@gmail.com>
2021-10-21 16:51:10 +0900

    created login screen

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { StyleSheet, Text, View, ScrollView } from "react-native";
+import { StyleSheet, Text, View, ScrollView, Button } from "react-native";
 import Activity from "./components/activity";
 import Profile from "./components/profile";
+
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
 
 import * as firebase from "firebase";
 
@@ -14,20 +16,44 @@ import "firebase/auth";
 
 // Initialize Firebase
 const firebaseConfig = {
-  // firebaseから発行される
+  apiKey: "AIzaSyAGR0DZVdwD2V-YFEc59UGi7zW7l41uOpg",
+  authDomain: "task-manager-e867b.firebaseapp.com",
+  projectId: "task-manager-e867b",
+  storageBucket: "task-manager-e867b.appspot.com",
+  messagingSenderId: "165006107797",
+  appId: "1:165006107797:web:56851f7f0a0782d152279c",
+  measurementId: "G-ME3QPCLP7H",
 };
 
 firebase.initializeApp(firebaseConfig);
 
 export default function App() {
+  // var provider = new firebase.auth.GoogleAuthProvider();
+
+  async function onLogin() {
+    console.log("click");
+
+    // Get the users ID token
+    const { idToken } = await GoogleSignin.signIn();
+
+    // Create a Google credential with the token
+    const googleCredential =
+      firebase.auth.GoogleAuthProvider.credential(idToken);
+
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(googleCredential);
+  }
+
   return (
     <ScrollView>
       <View style={styles.container}>
-        <Profile />
+        <Text>Testing Firebase!!!</Text>
+        <Button onPress={onLogin} title="Click Me" />
+        {/* <Profile />
         <Activity activity="Work" color="hsl(15, 100%, 70%)" />
         <Activity activity="Study" color="hsl(195, 74%, 62%)" />
         <Activity activity="Personal Projects" color="hsl(145, 58%, 55%)" />
-        <Activity activity="Exercise" color="hsl(348, 100%, 68%)" />
+        <Activity activity="Exercise" color="hsl(348, 100%, 68%)" /> */}
       </View>
     </ScrollView>
   );
