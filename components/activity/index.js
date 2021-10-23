fa5d03c822fbed56d23040b0c4235599a54f58e9
Rafael Viana <vianarafaelds@gmail.com>
2021-10-23 13:35:11 +0900

    added hidden element functionality

--- components/activity/index.js
+++ components/activity/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function Activity({ color, activity, data }) {
+  const [showTimer, setShowTimer] = React.useState(false);
   return (
     <View style={{ backgroundColor: color, borderRadius: 25, marginTop: 20 }}>
       <View style={styles.container}>
@@ -10,10 +11,18 @@ export default function Activity({ color, activity, data }) {
           <Text style={{ color: "#fff" }}>32 hrs</Text>
         </View>
         <View>
-          <Text style={{ color: "#fff" }}>...</Text>
+          <Text
+            style={{ color: "#fff" }}
+            onPress={() => {
+              setShowTimer(!showTimer);
+            }}
+          >
+            ...
+          </Text>
           <Text style={{ color: "#fff" }}>Last Week - 10 hrs</Text>
         </View>
       </View>
+      {showTimer && <Text style={{ color: "#fff" }}>Show Me</Text>}
     </View>
   );
 }
