232eb549825002385262592820eec916b2387917
Rafael Viana <vianarafaelds@gmail.com>
2021-10-24 14:15:20 +0900

    fixed the timer

--- components/activity/index.js
+++ components/activity/index.js
@@ -1,31 +1,38 @@
 import React from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
 
+String.prototype.toHHMMSS = function () {
+  var sec_num = parseInt(this, 10); // don't forget the second param
+  var hours = Math.floor(sec_num / 3600);
+  var minutes = Math.floor((sec_num - hours * 3600) / 60);
+  var seconds = sec_num - hours * 3600 - minutes * 60;
+
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+  return hours + ":" + minutes + ":" + seconds;
+};
+
 export default function Activity({ color, activity, data }) {
   const [showTimer, setShowTimer] = React.useState(false);
-  const [timer, setTimer] = React.useState({
-    seconds: 0,
-    minutes: 0,
-    hours: 0,
-    timer: 0,
-    id: null,
-  });
+  const [timer, setTimer] = React.useState(0);
   const [ID, setID] = React.useState(null);
 
   function add() {
-    if (timer.seconds === 60) {
-      setTimer({ ...timer, seconds: 0, minutes: timer.minutes++ });
-    } else if (timer.minutes === 60) {
-      setTimer({ ...timer, minutes: 0, hours: timer.hours++ });
-    } else {
-      setTimer({ ...timer, seconds: timer.seconds++ });
-    }
+    setTimer((timer) => timer + 1);
   }
 
   function startTimer() {
     const intervalID = setInterval(add, 1000);
     setID(intervalID);
   }
+
   return (
     <View style={{ backgroundColor: color, borderRadius: 25, marginTop: 20 }}>
       <View style={styles.container}>
@@ -46,9 +53,9 @@ export default function Activity({ color, activity, data }) {
       </View>
       {showTimer && (
         <View style={{ color: "#fff" }}>
-          <Text
-            style={{ color: "#fff", textAlign: "center", padding: 6 }}
-          >{`${timer.hours} : ${timer.minutes} : ${timer.seconds}`}</Text>
+          <Text style={{ color: "#fff", textAlign: "center", padding: 6 }}>
+            {String(timer).toHHMMSS()}
+          </Text>
           <Button onPress={startTimer} title="Start" />
           <Button
             onPress={() => {
